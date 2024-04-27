3f1cadfecd1250f1840fd84a2f7ea3496c28891e
Your Name <you@example.com>
2024-04-26 18:47:16 -0700

    environ

--- app/api/scrape/merchant/route.js
+++ app/api/scrape/merchant/route.js
@@ -5,8 +5,9 @@ export async function GET(request) {
   const { query } = parse(request.url, true);
   const maxPrice = query.maxPrice;
 
-    const flaskApiUrl = `https://racquetfinder-8c8b6e21eb36.herokuapp.com/api/scrape/merchant?maxPrice=${maxPrice}`;
-  
+    const flaskApiUrl = `${process.env.NEXT_PUBLIC_MERCHANT}?maxPrice=${maxPrice}`;
+    // `${process.env.NEXT_PUBLIC_DISPLAY_FETCH_QUIZ}?questionId=${questionId}`
+    // `https://racquetfinder-8c8b6e21eb36.herokuapp.com/api/scrape/merchant?maxPrice=${maxPrice}`
     try {
       const flaskResponse = await fetch(flaskApiUrl);
       const data = await flaskResponse.json();
